8c690498d541d8b0b6b8536668dfa73cdfc23775
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-11-13 16:23:54 +0700

    Upload data of small trees and make load_function

--- model/model_AI.py
+++ model/model_AI.py
@@ -1,26 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import cv2
 import glob
 from tkinter import *
 from PIL import Image, ImageTk
+class Tree_Model():  
+    def load_image():
+        #Function to load image
+        data_image = []
+        for filename in glob.glob('../images/*.jpg'):
+            image = Image.open(filename)
+            data_image.append(image)
 
-data_image = []
+    
 
-def load_data():
-    for filename in glob.glob('../images/*.jpg'):
-        image = Image.open(filename)
-        data_image.append(image)
+    def load_data():
+        #Function to load data
+        new_data = []
+        path_to_data = '../data/train/png'
+        for filename in glob.glob(path_to_data + '/*.png'):
+            image = cv2.imread(filename)
+            image = cv2.resize(image, (100, 100))
+            new_data.append(image)
+        new_data = np.array(new_data)
+        print("Data loaded successfully")
 
 
-def plot_data():
-    for image in data_image:
-        image.show()
-        render = ImageTk.PhotoImage(image)
-        img = Label(image=render)
-        img.image = render
-        img.place(x=0, y=0)
-
-def main():
-    load_data()
-    plot_data()
-
-if __name__ == '__main__':
-    main()
+    def plot_image():
+        #Function to plot image
+        for image in data_image:
+            image.show()
+            render = ImageTk.PhotoImage(image)
+            img = Label(image=render)
+            img.image = render
+            img.place(x=0, y=0)
--- ui/main.py
+++ ui/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import sys
 sys.path.append('C:\\Users\\Nguyen Quang Huy\\OneDrive\\Máy tính\\Tree_Classification_with_Image\\model')
 import model_AI
+from model_AI import *
 
 #Define app main
 main_app = Tk()
@@ -9,11 +10,20 @@ main_app.title("Tree Classification with Image")
 main_app.geometry("600x600")
 
 #Define image box
-load_image = Button(main_app, text="Load Image", command=model_AI.load_data)
+load_image = Button(main_app, text = "Load Image", command = Tree_Model.load_image)
 load_image.pack(pady = 20)
 
-plot_image = Button(main_app, text="Plot Image", command=model_AI.plot_data)    
+#Define data box
+load_data = Button(main_app, text = "Load Data", command = Tree_Model.load_data)
+load_data.pack(pady = 20)
+
+#Define plot image box
+plot_image = Button(main_app, text = "Plot Image", command = Tree_Model.plot_image)    
 plot_image.pack()
+
+#Define output listbox
+output_listbox = Listbox(main_app, width = 50)
+output_listbox.pack(pady = 20)
 
 #Run code 
 main_app.mainloop()
