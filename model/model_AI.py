75f3d8a3fb9171022644e3d72ecfba7ddd0897fd
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-12-10 14:02:36 +0700

    Define generate image function and add another dataset

--- model/model_AI.py
+++ model/model_AI.py
@@ -7,9 +7,13 @@ import glob
 from tkinter import *
 from PIL import Image, ImageTk
 from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import tensorflow as tf
 
-
-class Tree_Model:  
+class Tree_Data:  
     def load_image():
         #Function to load image
         data_image = []
@@ -56,20 +60,22 @@ class Tree_Model:
         print("Train data loaded successfully")
         train_data = pd.DataFrame(train_data)
         train_data.to_csv('./data/train.csv', index=False)
-        print("Train.csv created successfully")
 
     def generate_tree_image():
-        #Function to generate 10 tree data images from images folder into new_dataset folder
+        #Delete all images in train folder of new_dataset
+        for filename in glob.glob('./new_dataset/train/*.png'):
+            os.remove(filename)
+        #Function to generate 10 tree data images from images folder into train folder of new_dataset
         data_image = []
         for filename in glob.glob('./images/*.jpg'):
             image = Image.open(filename)
             data_image.append(image)
-        
+        image_names = ['orange_tree', 'Silver_birch']
         for i in range(10):
-            for image in data_image:
-                image = image.resize((70, 70))
+            for image , name in zip(data_image, image_names):
+                image = image.resize((299, 299))
                 image = np.array(image)
-                image = image.reshape((1, 70, 70, 3))
+                image = image.reshape((1, 299, 299, 3))
                 datagen = ImageDataGenerator(
                     rotation_range=10,
                     width_shift_range=0.1,
@@ -78,9 +84,102 @@ class Tree_Model:
                     horizontal_flip=True,
                     fill_mode='nearest')
                 j = 0
-                for batch in datagen.flow(image, batch_size=1, save_to_dir='./new_dataset', save_prefix='tree', save_format='jpg'):
+                #Save the image with corresponding name and format
+                for batch in datagen.flow(image, batch_size=1, save_to_dir='./new_dataset/train/', save_prefix = name, save_format = 'png'):
                     j += 1
                     if j == 1:
                         break
-        print("Tree images generated successfully")
+#Set parameters
+batch_size = 64
+epochs = 30
+num_classes = 10
+
+def load_data():
+    #Load data from train.csv
+    train_data = pd.read_csv('./data/train.csv')
+    train_data = train_data.sample(frac=1).reset_index(drop=True)
+    train_data.head()
+
+    #Split data into train and test
+    train = train_data[:3800]
+    test = train_data[3800:]
+
+    #Split data into X_train, y_train, X_test, y_test
+    X_train = train.iloc[:, :-1].values
+    y_train = train.iloc[:, -1].values
+    X_test = test.iloc[:, :-1].values
+    y_test = test.iloc[:, -1].values
+
+    #Reshape data
+    X_train = X_train.reshape(X_train.shape[0], 70, 70, 3)
+    X_test = X_test.reshape(X_test.shape[0], 70, 70, 3)
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+
+    #Normalize data
+    X_train /= 255
+    X_test /= 255
+
+    #Convert class vectors to binary class matrices
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+    return X_train, y_train, X_test, y_test
+
+def build_model():
+    #Build model
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=(299, 299, 3)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    #Save model
+    model.save('project_model.h5')
+    return model
+
+#Evaluate model
+def evaluate_model(model, X_train, y_train, X_test, y_test):
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test), shuffle=True)
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+#Prediction 
+def predict(model):
+    #Load image
+    image = Image.open('./images/1.jpg')
+    image = image.resize((299, 299))
+    image = np.array(image)
+    image = image.reshape((1, 299, 299, 3))
+    image = image.astype('float32')
+    image /= 255
+
+    #Predict image
+    result = model.predict(image)
+    result = np.argmax(result, axis=1)
+    print(result)
+
+    #Show image
+    plt.imshow(image.reshape(299, 299, 3))
+    plt.show()
+
 
--- ui/main.py
+++ ui/main.py
@@ -10,19 +10,19 @@ main_app.title("Tree Classification with Image")
 main_app.geometry("600x600")
 
 #Define load image button
-load_image = Button(main_app, text = "Load Image", command = Tree_Model.load_image)
+load_image = Button(main_app, text = "Load Image", command = Tree_Data.load_image)
 load_image.pack(pady = 20)
 
 #Define show train image button
-load_data = Button(main_app, text = "Load Data", command = Tree_Model.train_image)
+load_data = Button(main_app, text = "Load Data", command = Tree_Data.train_image)
 load_data.pack(pady = 20)
 
 #Define train csv file box
-train_csv = Button(main_app, text = "Create train csv file", command = Tree_Model.make_train_csv)
+train_csv = Button(main_app, text = "Create train csv file", command = Tree_Data.make_train_csv)
 train_csv.pack(pady = 20)
 
 #Define generate tree image button 
-generate_tree_image = Button(main_app, text = "Generate tree image", command = Tree_Model.generate_tree_image)
+generate_tree_image = Button(main_app, text = "Generate tree image", command = Tree_Data.generate_tree_image)
 generate_tree_image.pack(pady = 20)
 
 #Define output listbox
